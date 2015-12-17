35f99ee4d89c332a6abb5457499f817c031c82c8
Blue <zzx094@gmail.com>
2015-12-17 17:01:23 +0800

    first commit

---
+++ wifi-lock-screen/Lock.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace wifi_lock_screen
+{
+    partial class Lock
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_lock = new System.Windows.Forms.PictureBox();
+            this.idleCheck = new System.Windows.Forms.Timer(this.components);
+            this.unlockCheck = new System.Windows.Forms.Timer(this.components);
+            this.keepTop = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.btn_lock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_lock
+            // 
+            this.btn_lock.BackColor = System.Drawing.Color.Transparent;
+            this.btn_lock.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btn_lock.Image = global::wifi_lock_screen.Properties.Resources.lock_24px;
+            this.btn_lock.Location = new System.Drawing.Point(0, 0);
+            this.btn_lock.Name = "btn_lock";
+            this.btn_lock.Size = new System.Drawing.Size(284, 261);
+            this.btn_lock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btn_lock.TabIndex = 0;
+            this.btn_lock.TabStop = false;
+            this.btn_lock.Click += new System.EventHandler(this.btn_lock_Click);
+            this.btn_lock.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn_lock_MouseClick);
+            // 
+            // idleCheck
+            // 
+            this.idleCheck.Enabled = true;
+            this.idleCheck.Interval = 5000;
+            this.idleCheck.Tick += new System.EventHandler(this.idleCheck_Tick);
+            // 
+            // unlockCheck
+            // 
+            this.unlockCheck.Enabled = true;
+            this.unlockCheck.Interval = 5000;
+            this.unlockCheck.Tick += new System.EventHandler(this.unlockCheck_Tick);
+            // 
+            // keepTop
+            // 
+            this.keepTop.Enabled = true;
+            this.keepTop.Interval = 1;
+            this.keepTop.Tick += new System.EventHandler(this.keepTop_Tick);
+            // 
+            // Lock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btn_lock);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Lock";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.btn_lock)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox btn_lock;
+        private System.Windows.Forms.Timer idleCheck;
+        private System.Windows.Forms.Timer unlockCheck;
+        private System.Windows.Forms.Timer keepTop;
+    }
+}
+
---
+++ wifi-lock-screen/Lock.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace wifi_lock_screen
+{
+    public partial class Lock : Form
+    {
+        long lock_time = 60;
+        string unlock_mac = "90-67-1c-4c-55-42";
+        string unlock_ip = "";
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct LASTINPUTINFO
+        {
+            [MarshalAs(UnmanagedType.U4)]
+            public int cbSize;
+            [MarshalAs(UnmanagedType.U4)]
+            public uint dwTime;
+        }
+        [DllImport("user32.dll")]
+        public static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        public Lock()
+        {
+            InitializeComponent();
+        }
+
+        public void GetUnlockMac()
+        {
+            StreamReader sr = new StreamReader("unlock_mac.txt", Encoding.Default);
+            unlock_mac = sr.ReadLine();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            unlock_ip = GetMacIP(unlock_mac);
+        }
+
+        public long GetIdleTick()
+        {
+            LASTINPUTINFO vLastInputInfo = new LASTINPUTINFO();
+            vLastInputInfo.cbSize = Marshal.SizeOf(vLastInputInfo);
+            if (!GetLastInputInfo(ref vLastInputInfo)) return 0;
+            return (Environment.TickCount - (long)vLastInputInfo.dwTime) / 1000;
+        }
+
+        public bool IPAvailable(string ip)
+        {
+            Ping ping = new Ping();
+            try
+            {
+                PingReply pr = ping.Send(ip);
+                if (pr == null)
+                {
+                    return false;
+                }
+                else return pr.Status == IPStatus.Success;
+            }
+            catch { return false; }
+
+        }
+
+
+        public string GetMacIP(string mac)
+        {
+            mac = mac.ToLower();
+            string arp_list = CMD("arp -a");
+            Match mc = Regex.Match(arp_list, @"\S*(?=\s*" + mac + ")");
+            return mc.Value;
+        }
+        public string CMD(string str)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.Start();
+            p.StandardInput.WriteLine(str + "&exit");
+            p.StandardInput.AutoFlush = true;
+            //p.StandardInput.WriteLine("exit");
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            p.Close();
+            return output;
+        }
+
+
+        private void idleCheck_Tick(object sender, EventArgs e)
+        {
+
+            if (GetIdleTick() >= lock_time)
+            {
+                if (IPAvailable(unlock_ip) == false)
+                {
+                    this.Show();
+                }
+            }
+        }
+
+        private void unlockCheck_Tick(object sender, EventArgs e)
+        {
+            if (this.Visible == false) return;
+            unlock_ip = GetMacIP(unlock_mac);
+            if (IPAvailable(unlock_ip))
+            {
+                this.Hide();
+            }
+
+        }
+
+        private void keepTop_Tick(object sender, EventArgs e)
+        {
+            if (this.Visible)
+            {
+                //this.TopMost = true;
+                this.Focus();
+            }
+        }
+
+        private void btn_lock_MouseClick(object sender, MouseEventArgs e)
+        {
+            //Unlock Test
+            if (e.Button == MouseButtons.Middle)
+            {
+                this.Hide();
+            }
+        }
+
+        private void btn_lock_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
